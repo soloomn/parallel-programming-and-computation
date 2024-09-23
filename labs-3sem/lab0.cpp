aab580481669c079f92c41579d1bb5ec7c8e44ba
Tim <resn2p@gmail.com>
2024-09-23 21:51:07 +0300

    Create lab0.cpp

---
+++ labs-3sem/lab0.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "mpi.h"
+
+using namespace std;
+
+int mainlab0(int* argc, char** argv)
+{
+	int numtasks, rank;
+
+	MPI_Init(argc, &argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+	printf("Hello MPI from process = %d, total number of processes: %d\n", rank, numtasks);
+
+	MPI_Finalize();
+
+	return 0;
+}
